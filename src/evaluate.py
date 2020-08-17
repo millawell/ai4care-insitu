cc1a5c2f024660e5ca615ea05460162c1bf8ad10
David Lassner <davidlassner@gmail.com>
2020-08-17 10:33:44 +0200

    evaluation with precision and recall @k and ungrouping as part of the train.py. Also, train.py runs the baseline and the datathon solution.

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,9 +1,62 @@
+import warnings
 import click
 import json
 import configuration as config
 
 import numpy as np
 import pandas as pd
+
+
+def recall_at_k(true, pred, k):
+
+    if any(map(lambda x:len(x)==0, true)):
+        warnings.warn(f"Some samples have fewer no true labels.")
+
+    scores = []
+    for true_, pred_ in zip(true, pred):
+        if len(true_) == 0:
+            scores.append(0)
+        else:
+            scores.append(
+                len(set(pred_[:k]) & set(true_))
+                / len(true_)
+            )
+
+    return np.mean(scores)
+
+def precision_at_k(true, pred, k):
+
+    if any(map(lambda x:len(x)<k, pred)):
+        warnings.warn(f"Some samples have fewer than `k={k}` predictions.")
+
+    scores = []
+    for true_, pred_ in zip(true, pred):
+        if len(pred_) == 0:
+            scores.append(0)
+        else:
+            scores.append(
+                len(set(pred_[:k]) & set(true_))
+                / len(pred_[:k])
+            )
+
+    return np.mean(scores)
+
+
+def report(true, pred):
+
+    for k in [1,2,3,5,10]:
+        print("precision at k={}: {:.5f}".format(
+            k,
+            precision_at_k(true, pred, k)
+        ))
+    
+    for k in [1,2,3,5,10]:
+        print("recall at k={}: {:.5f}".format(
+            k,
+            recall_at_k(true, pred, k)
+        ))
+    
+
 
 @click.command()
 @click.option(
@@ -27,18 +80,9 @@ def main(predictions, groundtruth):
     groundtruth = pd.read_csv(groundtruth, sep="\t")
     groundtruth["Mitarbeiter ID"] = groundtruth["Mitarbeiter ID"].apply(json.loads)
 
-    scores = []
-    for (_,pred), (_,true) in zip(predictions.iterrows(), groundtruth.iterrows()):
-        scores.append(
-            len(set(pred["Mitarbeiter ID"]) & set(true["Mitarbeiter ID"]))
-            / len(set(pred["Mitarbeiter ID"]) | set(true["Mitarbeiter ID"]))
-        )
+    true = groundtruth["Mitarbeiter ID"].tolist()
+    pred = predictions["Mitarbeiter ID"].tolist()
 
-    print("accuarcy: {:.5f}".format(
-        np.mean(scores)
-    ))
-
-    
-
+    report(true, pred)
 if __name__ == '__main__':
     main()
--- src/train.py
+++ src/train.py
@@ -2,10 +2,7 @@ import click
 import json
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score, precision_score
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
@@ -17,9 +14,10 @@ import numpy as np
 import pandas as pd
 
 import configuration as config
+from evaluate import report
 
 
-class Model:
+class BaselineModel:
 
     def fit(self, data):
 
@@ -39,7 +37,7 @@ class Model:
             "Qualifikationgruppe"
         ])
 
-        self.num_offers = 4 ##int(self.data["Mitarbeiter ID"].apply(len).mean())
+        self.num_offers = int(self.data["Mitarbeiter ID"].apply(len).mean())
         
         return self
 
@@ -72,43 +70,94 @@ class Model:
             it for ll in subset.sort_values("days_dist")[::-1]["Mitarbeiter ID"] 
                 for it in ll]
         return y_pred[:self.num_offers]
-        
-
-        
 
 
+def ungroup(data):
+    
+    ungroup = []
+    for _, row in tqdm(data.iterrows(), desc="ungroup", total=len(data)):
+        for ma_id in row["Mitarbeiter ID"]:
+            ungroup.append(
+                row.to_dict()
+            )
+            ungroup[-1]["Mitarbeiter ID"] = ma_id
 
-#def train(data):
-#    '''Dummy trainer that always predicts the same k employee ids'''
-#
-#
-#    model = Model()
-#    model.fit(data)
-#
-#    return model
+    data = pd.DataFrame(ungroup)
 
-def train(data):
-    X_train = data.loc[:, data.columns != 'Mitarbeiter.ID']
-    y_train = data['Mitarbeiter.ID']
+    return data
+
+
+def run_baseline(train, test):
+
+    model = BaselineModel()
+    model.fit(train)
+
+    predictions = []
+    for _, row in tqdm(test.iterrows(), desc="prediction", total=len(test)):
+        predictions.append(model.predict(row))
+    
+    print("## BASELINE ## ")
+    report(
+        test["Mitarbeiter ID"].tolist(),
+        predictions
+    )
+
+
+def run_datathon_solution(train, test):
+
+    X_train = train.loc[:, train.columns != 'Mitarbeiter ID']
+    y_train = train['Mitarbeiter ID']
 
     # Transformation Steps
     numeric_features = ['Tag']
     numeric_transformer = Pipeline(steps=[('scaler', StandardScaler())])
 
-    categorical_features = ['Einsatzort', 'PLZ', 'Qualifikation', 'Qualifikationgruppe', 'Schicht','Wochentag','Feiertag']
-    categorical_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
+    categorical_features = [
+        'Einsatzort',
+        'PLZ',
+        'Qualifikation',
+        'Qualifikationgruppe',
+        'Schicht',
+        'Wochentag',
+        'Feiertag'
+    ]
+
+    categorical_transformer = Pipeline(steps=[
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
 
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features),
-            ('cat', categorical_transformer, categorical_features)])
+            ('cat', categorical_transformer, categorical_features)
+        ]
+    )
     
     SVC_pipeline = Pipeline([('preprocessor', preprocessor),
-                ('clf', OneVsRestClassifier(SGDClassifier(loss="log"), n_jobs=1))
-            ])
-    SVC_pipeline.fit(X_train,y_train)
-    return SVC_pipeline
+        ('clf', OneVsRestClassifier(SGDClassifier(loss="log"), n_jobs=-1))
+    ])
 
+    model = SVC_pipeline.fit(X_train,y_train)
+
+
+    predictionsprob = model.predict_proba(test)
+    mitarbeiter = model.steps[1][1].classes_
+    predictions = []
+    for index,i in enumerate(predictionsprob):
+        mitarbeiter_list = []
+        for j in i.argsort()[::-1][:10]: #Get highest probabilities
+                        
+            if i[j]>=0.0008: #threshold for probability of mitarbeiter.  
+                mitarbeiter_list.append(mitarbeiter[j])
+        predictions.append(mitarbeiter_list)
+
+    print("## Datathon solution ## ")
+    report(
+        test["Mitarbeiter ID"].tolist(),
+        predictions
+    )
+    return predictions
+    
 
 @click.command()
 @click.option(
@@ -134,39 +183,58 @@ def train(data):
 )
 def main(trainset, testset, output):
 
-    trainset = pd.read_csv(trainset, sep="\t")
-    #trainset["Mitarbeiter ID"] = trainset["Mitarbeiter ID"].apply(json.loads)
+    trainset = pd.read_csv(trainset, sep="\t").sample(frac=1).reset_index(drop=True)
+    trainset["Mitarbeiter ID"] = trainset["Mitarbeiter ID"].apply(json.loads)
     trainset.loc[:, "Tag"] = trainset["Tag"].apply(date.fromisoformat)
     min_day = trainset.Tag.min()
     trainset.loc[:, "Tag"] = trainset.Tag.apply(lambda d: (d - min_day).days)
-    
-    testset = pd.read_csv(testset, sep="\t")
 
+    testset = pd.read_csv(testset, sep="\t")
     testset.loc[:, "Tag"] = testset["Tag"].apply(date.fromisoformat)
     testset.loc[:, "Tag"] = testset.Tag.apply(lambda d: (d - min_day).days)
-	trainset.drop(['Unnamed: 0','X.1','X'],axis=1,inplace=True) # remove weird columns from preprocessing of training data 
+    testset["Mitarbeiter ID"] = testset["Mitarbeiter ID"].apply(json.loads)
     
+    # For Debug 
+    # trainset = trainset.sample(1000)
+    # testset = testset.sample(1000)
 
-    model = train(trainset)
-    predictionsprob = model.predict_proba(testset)
-    mitarbeiter = model.steps[1][1].classes_
-    solutions = {}
-    for index,i in enumerate(predictionsprob):
-        mitarbeiter_list = []
-        for j in i.argsort()[-8:]: #Get 8 highest probabilities
-                        
-            if i[j]>=0.0008: #threshold for probability of mitarbeiter.  
-                mitarbeiter_list.append(mitarbeiter[j])
-        solutions[index] = mitarbeiter_list
-    
-    pd.Series(solutions).rename("Mitarbeiter ID").to_csv(os.path.join('data','predictions.tsv'),sep="\t")
-    
-#    preds = []
-#    for _, row in tqdm(testset.iterrows(), desc="prediction", total=len(testset)):
-#        preds.append(model.predict(row))
-#    testset["Mitarbeiter ID"] = preds
+    ungrouped_trainset = ungroup(trainset)
 
-#    testset.to_csv(output, index=False, sep="\t")
+    run_baseline(trainset, testset)
+
+    predictions = run_datathon_solution(ungrouped_trainset, testset)
+
+    testset["Mitarbeiter ID"] = predictions
+
+    testset.to_csv(output, index=False, sep="\t")
+
+    # results on the "small" real world data set with 
+    # ~50k orders and ~500k offers in the training set
+    # 17k orders in the test set
+
+    # ## BASELINE ##
+    # precision at k=1: 0.15575
+    # precision at k=2: 0.12922
+    # precision at k=3: 0.11385
+    # precision at k=5: 0.09608
+    # precision at k=10: 0.08229
+    # recall at k=1: 0.07707
+    # recall at k=2: 0.10906
+    # recall at k=3: 0.12964
+    # recall at k=5: 0.15971
+    # recall at k=10: 0.20232
+
+    # ## Datathon solution ##
+    # precision at k=1: 0.28013
+    # precision at k=2: 0.25015
+    # precision at k=3: 0.22822
+    # precision at k=5: 0.20171
+    # precision at k=10: 0.17040
+    # recall at k=1: 0.05244
+    # recall at k=2: 0.08593
+    # recall at k=3: 0.11066
+    # recall at k=5: 0.14458
+    # recall at k=10: 0.20309
 
 if __name__ == '__main__':
     main()
